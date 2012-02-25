4d249679267361361b1459ce44b55e3f08627695
zhuanqs <zhuanqingshan@163.com>
2012-02-25 23:47:48 +0800

    quote and double-quote

---
+++ basic-types/types.rb
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#在ascii码前加？可以获取其整数值
+p ?a
+
+#Ctrl+x的组合键， 为x & 0x9f 的值
+p ?\C-
+
+p ?\C-x
+puts ?\C-a
+
+#Meta+x, 为x & 0x80 的值
+puts ?\M-
+
+puts ?\M-a
+
+# quote 
+p 'hello'
+puts 'hello'
+p 'a backslash \'\\\''
+puts 'a backslash \'\\\''
+p %q/simple string/
+p %q{a (bad) idea}
+p %q no_blank_here ;
+
+# double quote
+a = 123
+p "\123mile"
+p "Say \"Hello\""
+
+p %Q!"I said 'nuts', " i said!
