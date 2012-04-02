c75562e29b490e3d72055311cf9eeffffe0760f0
zhuanqs <zhuanqingshan@163.com>
2012-04-02 22:42:46 +0800

    override method_missing

---
+++ ruby-metaprog/methods/proxy.rb
@@ -0,0 +1,24 @@
+require './data_source.rb'
+
+class Computer
+  def initialize(computer_id, data_source)
+    @id = computer_id
+    @data_source = data_source
+  end
+
+  def method_missing(name, *args)
+    super if !@data_source.respond_to?("get_#{name}_info")
+    info = @data_source.send("get_#{name}_info", args[0])
+    price = @data_source.send("get_#{name}_price", args[0])
+    result = "#{name.to_s.capitalize}: #{info} ($#{price})"
+    p result
+  end
+
+  def respond_to?(method)
+    @data_source.respond_to?("get_#{method}_info") || super
+  end
+end
+
+c = Computer.new(42, DataSource.new)
+c.cpu
+p c.respond_to?(:mouse)
